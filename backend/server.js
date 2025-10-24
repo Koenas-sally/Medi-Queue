2fc1c2d1bc674642e41afa2b48a2146ff0e71f6f
Makoena Mabotja <makoenasally62@gmail.com>
2025-10-24 16:23:55 +0200

    import hospital route in server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import hospitalRoutes from "./routes/hospitalRoutes.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/hospitals", hospitalRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
